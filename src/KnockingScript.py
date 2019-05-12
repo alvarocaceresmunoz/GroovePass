edf9ee756f45014925686441e2c7ed6c2d87e427
Alvaro Caceres <alvarocaceresmunoz@gmail.com>
2019-05-12 17:50:24 +0100

    Sensors working on Anas board

---
+++ src/KnockingScript.py
@@ -0,0 +1,103 @@
+import grovepi
+import time
+import grove6axis
+
+#HIGH PASS VARIABLES
+loudnessHPF = 0
+loudnessLast = 0
+noiseHPF = 0
+noiseLast = 0
+constant = 0.1
+
+#NOISE/LOUDNESS VARIABLES
+zeroSound = True
+loudnessStart = 0
+noiseStart = 0
+noiseAvg = 0
+noiseThreshold = 0
+i = 0
+maxLoops = 500
+distanceMin = 30
+
+# port config constants
+portLoudness = 2
+portNoise = 0
+portDistance = 4
+
+# other constants
+userKeepsKnocking = True
+timeSinceLastKnock = 0
+timeSinceLastKnockMax = 40
+waitTime = 0.005
+
+grove6axis.init6Axis()
+
+
+print("[info] getting ambient noise...")
+while userKeepsKnocking:
+    #read from an analog sensor on input 0 (loudness sensor)
+    loudness = grovepi.analogRead(portLoudness)
+    #read from an analog sensor on input 1 (noise sensor)
+    noise = grovepi.analogRead(portNoise)
+    #read from a digital sensor (ultrasonic) on input 4
+    distance = grovepi.ultrasonicRead(portDistance)
+
+    acceleration = grove6axis.getAccel()
+    accelerationX = acceleration[0]
+    accelerationY = acceleration[1]
+    accelerationZ = acceleration[2]
+    accelerationCombined = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ)
+
+    if zeroSound == True:
+        while True:
+            #READ THE VALUES EACH RUN
+            #read from an analog sensor on input 1 (loudness sensor)
+            loudness = grovepi.analogRead(portLoudness)
+            #read from an analog sensor on input 1 (noise sensor)
+            noise = grovepi.analogRead(portNoise)
+
+            #Average the first 500 values to zero the input
+            loudnessStart = loudness + loudnessStart
+            noiseStart = noise + noiseStart
+            i += 1
+
+            # print("Loop %d: %4.2f, %4.2f"%(i,loudnessStart,noiseStart))
+
+            #Break out of the loop and calculate the average and threshold (25% above)
+            if i == maxLoops:
+                noiseAvg = (loudnessStart + noiseStart) / (i * 2)
+                noiseThreshold = noiseAvg / 3
+                # print("Average Sound: %4.2f, Upper Threshold: %4.2f"%(noiseAvg, noiseThreshold))
+                zeroSound = False
+                print("[info] ambient sensing done...")
+                time.sleep(1)
+                print("[info] Start listening in 1...")
+                time.sleep(1)
+                print("[info] 2...")
+                time.sleep(1)
+                print("[info] 3...")
+                time.sleep(1)
+                print("[info] Start knocking!")
+                print("time,distance,loudness,noise,noiseAvg,loudnessHPF,noiseHPF,knockDetected,timeSinceLastKnock,accelerationX,accelerationY,accelerationZ,accelerationCombined")
+                break
+
+    #Calculate the generic high pass filter for the loudness sensor
+    loudnessHPF = constant * (loudnessHPF + loudness - loudnessLast)
+    loudnessLast = loudness
+
+    #Calculate the generic high pass filter for the noise sensor
+    noiseHPF = constant * (noiseHPF + noise - noiseLast)
+    noiseLast = noise
+
+    #If the high pass is above 4 and the sensor readings are above the average threshold, knocking is occuring
+    if loudness > (noiseAvg + noiseThreshold): # and distance < distanceMin:
+        knockDetected = True
+        timeSinceLastKnock = 0
+    else:
+        knockDetected = False
+        timeSinceLastKnock += 1
+
+    print("%4.2f, %4.2f, %4.2f, %4.2f, %4.2f, %4.2f, %4.2f, %s, %d, %f, %f, %f, %f"%(time.time(),distance,loudness,noise,noiseAvg,loudnessHPF,noiseHPF,knockDetected,timeSinceLastKnock,accelerationX,accelerationY,accelerationZ,accelerationCombined))
+
+    # if timeSinceLastKnock > timeSinceLastKnockMax:
+    #     userKeepsKnocking = False
--- src/run.py
+++ src/run.py
@@ -3,38 +3,46 @@ import time
 import grove6axis
 
 inputPortDistance = 4
+inputPortTouch = 3
+grovepi.pinMode(inputPortTouch,'INPUT')
 inputPortLoudness = 2
 grove6axis.init6Axis()
 
-nearDistanceThreshold = 60
+# nearDistanceThreshold = 60
 typeTime = '%f'
 typeLoudness = '%d'
 typeDistance = '%d'
+typeTouch = '%d'
 typeOrientation = '%f'
 typeAcceleration = '%f'
-sensorVariables = [typeTime, typeDistance, typeLoudness, typeOrientation, typeOrientation, typeOrientation, typeAcceleration, typeAcceleration, typeAcceleration]
-headerString = 'time, distance, loudness, orientationX, orientationY, orientationZ, accelerationX, accelerationY, accelerationZ'
+sensorVariables = [typeTime, typeDistance, typeLoudness, typeTouch, typeOrientation, typeOrientation, typeOrientation, typeAcceleration, typeAcceleration, typeAcceleration]
+header = 'time, distance, loudness, touch, orientationX, orientationY, orientationX, accelerationX, accelerationY, accelerationZ'
 rowFormatString = ','.join(sensorVariables)
 
-waitTime = 0.001
+waitTime = 0.1
 
-print(headerString)
+print(header)
 while(True):
     timestamp = time.time()
-    loudness = grovepi.analogRead(inputPortLoudness)
     distance = grovepi.ultrasonicRead(inputPortDistance)
+    loudness = grovepi.analogRead(inputPortLoudness)
+    touch = grovepi.digitalRead(inputPortTouch)
+
     orientation = grove6axis.getOrientation()
     orientationX = orientation[0]
     orientationY = orientation[1]
     orientationZ = orientation[2]
+
     acceleration = grove6axis.getAccel()
     accelerationX = acceleration[0]
     accelerationY = acceleration[1]
     accelerationZ = acceleration[2]
 
+    print(rowFormatString % (timestamp, distance, loudness, touch, orientationX, orientationY, orientationX, accelerationX, accelerationY, accelerationZ))
+
     # print(rowFormatString % (timestamp, distance, loudness, orientationX, orientationY, orientationZ, accelerationX, accelerationY, accelerationZ))
-    if distance < nearDistanceThreshold:
-        print('%d: near!', distance)
-    else:
-        print('%d: far...', distance)
+    # if distance < nearDistanceThreshold:
+    #     print('%d: near!', distance)
+    # else:
+    #     print('%d: far...', distance)
     time.sleep(waitTime)
